1b2166c76489e51d627aeb30780f8abe4d62c0a5
Agneta <agnetaomondi4@gmail.com>
2025-08-25 14:17:48 +0300

    Creating a superuser to use JWT token

--- reminders/models.py
+++ reminders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Reminder(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reminders")
+    user = models.ForeignKey( User, on_delete=models.CASCADE, related_name="reminders", null=True)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     due_date = models.DateTimeField()
